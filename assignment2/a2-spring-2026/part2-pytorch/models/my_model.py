6c7acb5be74dea036337de285a102bdbb2087ef6
Durga Harish Dayapule <durgaharish@outlook.com>
2026-02-17 23:11:56 -0800

    own model update

--- assignment2/a2-spring-2026/part2-pytorch/models/my_model.py
+++ assignment2/a2-spring-2026/part2-pytorch/models/my_model.py
@@ -35,21 +35,34 @@ def hello_do_you_copy():
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
-        #############################################################################
-        # TODO: Initialize the network weights                                      #
-        #############################################################################
-
-        #############################################################################
-        #                              END OF YOUR CODE                             #
-        #############################################################################
+        # Custom CNN: 2 conv layers, 2 pool, 2 fc
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.res_conv = nn.Conv2d(32, 64, kernel_size=1, stride=1, padding=0)  # for residual, stride=1
+        self.res_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        outs = None
-        #############################################################################
-        # TODO: Implement forward pass of the network                               #
-        #############################################################################
-
-        #############################################################################
-        #                              END OF YOUR CODE                             #
-        #############################################################################
-        return outs
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.pool1(out)
+        # Save for residual before conv2 and pool2
+        res = self.res_conv(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.pool2(out)
+        # Match spatial size of res to out
+        res = nn.functional.adaptive_max_pool2d(res, out.shape[2:])
+        out = out + res
+        out = self.flatten(out)
+        out = self.fc1(out)
+        out = self.relu3(out)
+        out = self.fc2(out)
+        return out
